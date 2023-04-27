06957d029d9148c9cbb9deba4974e6d33c7ee3c9
Mailton Junior <mpsjunior99@gmail.com>
2023-04-26 23:58:48 -0300

    pokedex

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/logo1.png">
+      <hr>
+      <input type="text"  placeholder="Buscar Pokemon Pelo Nome" v-model="busca"  class="input is-rounded">
+      <button class="button is-info is-rounded" id="btnBuscar" @click="buscar" >Buscar</button>
+      <div v-for="(poke,index) in resultadoBusca" :key="index">
+      <ComponentePokemon :name="poke.name" :url="poke.url" :num="index+1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
+
+import ComponentePokemon from './components/ComponentePokemon.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      pokemons: [],
+      filteredPokemons: [],
+      busca:'',
+    }
+  },
+  created: function(){
+   axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").then (res =>{
+    console.log("Pegou a lista de Pokemons");
+    this.pokemons = res.data.results;
+    this.filteredPokemons = res.data.results
+   })
+  },
   components: {
-    HelloWorld
+    ComponentePokemon
+  },
+
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+
+      }
   }
+},
+
+  computed:{
+    
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  }
+  
 }
 </script>
 
@@ -24,5 +72,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#btnBuscar{
+  width: 100%;
+  margin-top: 2%;
 }
 </style>
